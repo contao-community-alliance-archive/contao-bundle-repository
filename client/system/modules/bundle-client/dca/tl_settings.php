d03a9b4945551fca8d301bfbb8da0de1e1f6c51d
Stefan Lindecke <github.com@chektrion.de>
2011-12-01 15:27:47 +0100

    Pagination eingefuegt Manuelle Installation bezogen auf HashTag Uebersetzungen

---
+++ client/system/modules/bundle-client/dca/tl_settings.php
@@ -0,0 +1,29 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{areaBundles},bundle_server_requestUrl';
+$GLOBALS['TL_DCA']['tl_settings']['config']['onload_callback'][] = array('tl_bundle_client_settings','onload_callback');
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['bundle_server_requestUrl'] = array(
+	'label'		=>	&$GLOBALS['TL_LANG']['tl_settings']['bundle_server_requestUrl'],
+	'eval'		=>	array('rgxp'=>'url', 'mandatory'=>true),
+	'inputType'	=>	'text',
+);
+
+
+
+class tl_bundle_client_settings extends Backend
+{
+	public function onload_callback(DataContainer $dc)
+	{
+		if (!$GLOBALS['TL_CONFIG']['bundle_server_requestUrl'])
+		{
+			$this->import("Config");
+			
+			$GLOBALS['TL_CONFIG']['bundle_server_requestUrl']='http://kunden.ktrion.de/vhosts/contao-bundle-server/';
+			$this->Config->update("\$GLOBALS['TL_CONFIG']['bundle_server_requestUrl']", $GLOBALS['TL_CONFIG']['bundle_server_requestUrl']);
+	
+			
+		}
+	}
+
+}
