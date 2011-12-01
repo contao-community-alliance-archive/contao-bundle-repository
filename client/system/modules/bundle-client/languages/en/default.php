c3f12f21ca016d74b4d4d52142007640d28242af
Stefan Lindecke <github.com@chektrion.de>
2011-12-01 16:13:50 +0100

    Verzeichniss umbenannt

--- client/system/modules/bundle-client/languages/en/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['tl_bundle_client_overview']['edit'] = 'Bundle Repository';
-$GLOBALS['TL_LANG']['MOD']['bundle_overview'] = array("Bundle Repository","");
-
-$GLOBALS['TL_LANG']['tl_bundle_client_overview']['bundle_install'] = array("Manual installation of BundlePackage","Enter the Hash Tag for the bundle","Install this bundle");
-$GLOBALS['TL_LANG']['tl_bundle_client_overview']['bundles_list'] = array("Available bundles","","Install bundle");
-$GLOBALS['TL_LANG']['tl_settings']['bundle_server_requestUrl'] = array("BundleServer URL","Enter the domainname for the bundle server repository");
---
+++ client/system/modules/bundle_client/languages/de/default.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['bundle_overview'] = array("Bundle Repository","");
+
+$GLOBALS['TL_LANG']['tl_bundle_client_overview']['bundle_install'] = array("Manuelle Installation eines BundlePacketes","Geben Sie den HASH Tag des Paketes an","Dieses Paket installieren");
+$GLOBALS['TL_LANG']['tl_bundle_client_overview']['bundles_list'] = array("Verfuegbare Pakete","","Installiere Paket");
+$GLOBALS['TL_LANG']['tl_settings']['areaBundles'] = "Bundle Repository";
+$GLOBALS['TL_LANG']['tl_settings']['bundle_server_requestUrl'] = array("Bundle Repository URL","Domain des Bundle Repositories");
